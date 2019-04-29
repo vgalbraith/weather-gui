5af1333d48161de1289e98dc300ff8c354ede922
Zed Chance <zchance@student.sierracollege.edu>
2019-04-29 16:34:50 -0700

    Removed gui.fxml and renamed gui-2.fxml to gui.fxml

--- src/zchance/Main.java
+++ src/zchance/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("gui-2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("Hello Weather");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
