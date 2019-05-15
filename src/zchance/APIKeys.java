e731a13628d9c31e3d5a63bb4a44dce88e3828b2
Zed Chance <zchance@student.sierracollege.edu>
2019-05-15 14:21:38 -0700

    Changed CLIENT_ID and CLIENT_SECRET to my own account

--- src/zchance/APIKeys.java
+++ src/zchance/APIKeys.java
@@ -2,6 +2,10 @@ package zchance;
 
 class APIKeys
 {
-   final static String CLIENT_ID = "wQhXMMnxoRV4HNKoRLZrL";
-   final static String CLIENT_SECRET = "JIZobWk2qyfbStTUJSShF1kTLp06WTXLcJKA5dpD";
+   final static String CLIENT_ID = "kk1CU9UdOTJ050z63ZuDh";
+   final static String CLIENT_SECRET = "1y2kRweAUVgYoanjcWypsGBfbHPezYTOyQMDARXK";
+
+   // Class IDs
+   // wQhXMMnxoRV4HNKoRLZrL
+   // JIZobWk2qyfbStTUJSShF1kTLp06WTXLcJKA5dpD
 }
